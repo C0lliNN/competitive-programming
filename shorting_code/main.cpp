5f59290e7c55aa574823a9e545e74b44edb8d082
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-05-29 12:30:05 -0300

    Shorting code

---
+++ shorting_code/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define F first;
+#define S second;
+#define PB push_back;
+#define MP make_pair;
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+
+using namespace std;
+
+int main() {
+  // Shorten long types. Similar to type declaration in golang
+  typedef long long ll;
+
+  ll a = 123456789;
+  ll b = 987654321;
+  cout << a*b << "\n";
+
+  // v.PB(MP(y1,x1));
+  // v.PB(MP(y2,x2));
+  // int d = v[i].F+v[i].S;
+
+  // REP(i,1,n) {
+  // search(i);
+  // }
+}
